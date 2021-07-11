bca208f0a769fdf46903d9eb476014e0238217d2
Yeshi Tsering Gangchen <yestsering@gmail.com>
2021-07-11 16:47:11 +0200

    json token, Login & register route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,81 @@
 const Users = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { findOne } = require('../models/userModel');
 
 const userCtrl = {
     register: async (req, res) => {
-        try{
+        try {
+            const { name, email, password } = req.body;
+            const user = await Users.findOne({ email });
+
+            if (user)
+                return res.status(401).json({ message: "Email already registered." })
+            if (password.length < 8)
+                return res.status(401).json({ message: "Password to short" })
+
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const newUser = await Users({
+                name, email, password: passwordHash
+            });
+            await newUser.save();
+
+            const accessToken = createAccessToken({ id: newUser._id });
+            const refreshToken = createRefreshToken({ id: newUser._id });
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+            res.status(200).json({ accessToken: accessToken });
+        } catch (err) {
+            return res.status(500).json({ message: err.message })
+        }
+    },
+    refreshToken: async (req, res) => {
+        try {
+            const rf_token = req.cookies.refreshToken;
+            if (!rf_token) return res.status(400).json({ message: "Please Login or Register" })
+
+            jwt.verify(rf_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) return res.status(401).json({ message: "Please Login or Register" })
+
+                const accessToken = createAccessToken({ id: user.id })
+                res.json({ user, accessToken });
+            })
+        } catch (err) {
+            return res.status(500).json({ message: err.message })
+        }
+    },
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+            const user = await Users.findOne({ email });
+            if (!user) return res.status(401).json({ message: "Users does not exist." })
+
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) return res.status(401).json({ message: "Incorrect password." })
+
+            const accessToken = createAccessToken({ id: user._id });
+            const refreshToken = createRefreshToken({ id: user._id });
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refresh_token'
+            })
+            console.log(accessToken)
+            res.status(200).json({ message: "Login success." })
         } catch (err) {
             return res.status(500).json({ message: err.message })
         }
     }
+}
+
+const createAccessToken = (user) => {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
 };
+const createRefreshToken = (user) => {
+    return jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' })
+}
 
 module.exports = userCtrl;
--- router/userRouter.js
+++ router/userRouter.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userCtrl = require("../controllers/userController");
 
 
-router.post('/register', userCtrl)
+router.post('/register', userCtrl.register);
+
+router.post('/login', userCtrl.login)
+router.get('/refresh_token', userCtrl.refreshToken)
 
 module.exports = router;
